b45633f3ea9e7562d444d9a3fbe22a4fc1525c7c
Dhruvina Ghodadara <162274778+dhruvighodadara17@users.noreply.github.com>
2024-06-17 19:48:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+document.getElementById("logo").addEventListener("click", function () {
+    location.reload();
+    window.location.href = "index.html";
+});
+document.getElementById("wishButton").addEventListener("click", function () {
+    window.location.href = "wishlist.html";
+})
+document.getElementById("cartButton").addEventListener("click", function () {
+    window.location.href = "cart.html";
+})
+document.getElementById("accountButton").addEventListener("click", function () {
+    window.location.href = "registration.html";
+})
+
+// document.getElementById("home").addEventListener("click", function () {
+//     location.reload();
+//     window.location.href = "index.html"
+// })
+
+document.getElementById("stationery-supply").addEventListener("click", function () {
+    window.location.href = "stationery.html";
+})
+
+document.getElementById("office").addEventListener("click", function () {
+    window.location.href = "office-supply.html";
+})
+document.getElementById("art").addEventListener("click", function () {
+    window.location.href = "artSupply.html";
+})
+
+document.getElementById("craft").addEventListener("click", function () {
+    window.location.href = "craftSupply.html";
+})
+
+// $('.carousel').carousel({
+//     interval: 2000
+// });
+document.getElementById("button-1").addEventListener("click", function () {
+    window.location.href = "penSection.html";
+})
+document.getElementById("button-2").addEventListener("click", function () {
+    window.location.href = "pencilSection.html";
+})
+document.getElementById("button-3").addEventListener("click", function () {
+    window.location.href = "notbookSection.html";
+})
+document.getElementById("button-4").addEventListener("click", function () {
+    window.location.href = "highliter.html";
+})
+document.getElementById("button-5").addEventListener("click", function () {
+    window.location.href = "stickyNote.html";
+})
+document.getElementById("button-6").addEventListener("click", function () {
+    window.location.href = "eraserSection.html";
+})
+document.getElementById("button-7").addEventListener("click", function () {
+    window.location.href = "colorSection.html";
+})
+document.getElementById("button-8").addEventListener("click", function () {
+    window.location.href = "craftSection.html";
+})
